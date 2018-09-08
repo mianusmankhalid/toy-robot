7e9f89962b442eef7b2b5e07754bc3d8962f606e
Usman Khalid <mianusman@msn.com>
2018-09-08 22:55:29 +0800

    aligns the table with the test cases

--- src/table/index.js
+++ src/table/index.js
@@ -7,4 +7,16 @@ export default class {
         this.valueX = dimensionX;
         this.valueY = dimensionY;
     }
+
+    /**
+    * Get the table boundaries
+    */
+   get Boundaries() {
+       return {
+           x1: 0,
+           y1: 0,
+           x2: this.valueX - 1,
+           y2: this.valueY - 1,
+       };
+   }
 }
--- test/table.test.js
+++ test/table.test.js
@@ -1,19 +1,22 @@
 import { expect } from "chai";
+import Table from "../src/table";
 
 describe("A true table", function() {
   describe("Given geometric coordinate system", function() {
     it("should have boundaries with x(+) and y(+)", function() {
-      let table = { x1: 0, x2: 10, y1: 0, y2: 10 };
+      let table = new Table(5,5);
+      let boundaries = table.Boundaries;
 
-      [table.x1, table.y1, table.x2, table.y2].forEach(value =>
+      [boundaries.x1, boundaries.y1, boundaries.x2, boundaries.y2].forEach(value =>
         expect(value).to.be.at.least(0)
       );
     });
 
     it("should be a positive square", function() {
-      let table = { x1: 0, x2: 10, y1: 0, y2: 10 };
+      let table = new Table(5,5);
+      let boundaries = table.Boundaries;
 
-      expect(table.x2).to.be.equal(table.y2);
+      expect(boundaries.x2).to.be.equal(boundaries.y2);
     });
   });
 });
