ffab949f8520bf8cbf1c3fe03ffcb020d4674fda
Usman Khalid <mianusman@msn.com>
2018-09-08 22:44:46 +0800

    #11 - Defines the basic table structure

---
+++ src/table/index.js
@@ -0,0 +1,10 @@
+/**
+ * Creating a table-top with dimensions X * Y for robot to move.
+ */
+export default class {
+
+    constructor(dimensionX, dimensionY) {
+        this.valueX = dimensionX;
+        this.valueY = dimensionY;
+    }
+}
