0b34183c95d60e313fef4e0e30ae7cf92fc6a024
Usman Khalid <mianusman@msn.com>
2018-09-10 19:21:29 +0800

    creates MOVE action executor

--- src/robot/executor/index.js
+++ src/robot/executor/index.js
@@ -4,16 +4,21 @@ import _ from "lodash";
 export default class {
   static ExecuteAction(text, controller) {
     let regex = {
-      place: /^\s*PLACE\s+([\d]+)\s*,\s*([\d]+)\s*,\s*(EAST|WEST|NORTH|SOUTH)\s*$/gim
+      place: /^\s*PLACE\s+([\d]+)\s*,\s*([\d]+)\s*,\s*(EAST|WEST|NORTH|SOUTH)\s*$/gim,
+      move: /^\s*MOVE\s*$/gim
     };
 
-    let place = regex.place.exec(text);
-    if (!_.isEmpty(place)) {
-      return controller.PlaceRobot(
-        _.toInteger(place[1]),
-        _.toInteger(place[2]),
-        Parse(place[3])
-      );
+    if (regex.move.exec(text)) {
+      return controller.Move();
+    } else {
+      let place = regex.place.exec(text);
+      if (!_.isEmpty(place)) {
+        return controller.PlaceRobot(
+          _.toInteger(place[1]),
+          _.toInteger(place[2]),
+          Parse(place[3])
+        );
+      }
     }
 
     return false;
